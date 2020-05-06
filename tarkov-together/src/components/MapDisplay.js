1ac67285a788d0f4301112e5388511dcd403983c
AA AA <armalyalbert@gmail.com>
2020-05-06 19:35:58 -0400

    Additions

---
+++ tarkov-together/src/components/MapDisplay.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class MapDisplay extends React.Component{
+
+    render(){
+        return(<><p>HI!</p></>);
+    }
+}
--- tarkov-together/src/components/MapSelection.js
+++ tarkov-together/src/components/MapSelection.js
@@ -2,14 +2,19 @@ import React from 'react';
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown';
 import "./MapSelection.css";
+import MapDisplay from './MapDisplay';
 
 class MapSelection extends React.Component{
 
+    handleSelect(evtKey){
+        console.log(evtKey);
+    }
+
     render(){
         return(<div className="mapDropDown">
-            <DropdownButton variant= "secondary" id="map-drop-down" title="Map">
+            <DropdownButton variant= "secondary" id="map-drop-down" title="Map" onSelect={this.handleSelect(eventKey)}>
                 {/* TODO: Later Dynamically pull names from DB/Bucket*/}
-                <Dropdown.Item eventKey="1">Customs</Dropdown.Item>
+                <Dropdown.Item location= "/test" eventKey="1">Customs</Dropdown.Item>
                 <Dropdown.Item eventKey="2">Shoreline</Dropdown.Item>
                 <Dropdown.Item eventKey="3">Woods</Dropdown.Item>
                 <Dropdown.Item eventKey="4">Interchange</Dropdown.Item>
