4ad1a4bba9ee4e834b4c9a66b0c441d954b13566
AA AA <armalyalbert@gmail.com>
2020-05-07 02:09:44 -0400

    Working test image

--- tarkov-together/src/controllers/Base.js
+++ tarkov-together/src/controllers/Base.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import MapSelection from '../components/MapSelection';
 import '../controllers/Base.css';
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import Customs from "../maps/customs.jpg";
 
 class Base extends React.Component{
     render(){
@@ -9,6 +12,10 @@ class Base extends React.Component{
         <div className="mapSelection">
             < MapSelection />
         </div>
+
+        <Container fluid>
+            <Image src={Customs} fluid />
+        </Container>
 
     </div>);
     }
