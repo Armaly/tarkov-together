092f3119d8f7e393a18c8e952ada466fc270c2cb
AA AA <armalyalbert@gmail.com>
2020-05-07 02:32:29 -0400

    -Game Selection Skeleton -Moved maps to actual map display -Made game specific map directories

--- tarkov-together/src/components/GameSelection.js
+++ tarkov-together/src/components/GameSelection.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
+import Tarkov from "../logos/tarkov-logo.jpg";
+import Valorant from "../logos/Valorant-logo.png";
 
 class GameSelection extends React.Component{
 
@@ -6,12 +10,14 @@ class GameSelection extends React.Component{
         return(<>
             <CardGroup>
                 <Card>
-                    
+                    <Card.Img src={Tarkov} />
                 </Card>
                 <Card>
-
+                    <Card.Img src={Valorant} />
                 </Card>
             </CardGroup>
         </>);
     }
 }
+
+export default GameSelection;
--- tarkov-together/src/controllers/Base.js
+++ tarkov-together/src/controllers/Base.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import MapSelection from '../components/MapSelection';
 import '../controllers/Base.css';
-import Image from 'react-bootstrap/Image'
-import Container from 'react-bootstrap/Container'
-import Customs from "../maps/customs.jpg";
+import GameSelection from '../components/GameSelection';
+
 
 class Base extends React.Component{
     render(){
         
         return(<div className="Base">
-        <div className="mapSelection">
-            < MapSelection />
-        </div>
-
-        <Container fluid>
-            <Image src={Customs} fluid />
-        </Container>
+      
+       <GameSelection />
 
     </div>);
     }
